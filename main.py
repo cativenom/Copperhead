4b8fec9e8986a309055455001b56da9eacf501fb
cativenom <zack.ziegenfus@gmail.com>
2025-11-15 20:18:44 -0500

    Intial

---
+++ main.py
@@ -0,0 +1,37 @@
+from google import genai
+import os
+from openai import OpenAI
+
+client = genai.Client()
+
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+)
+print("gemini: " + response.text)
+
+
+
+##Chatgpt
+#client = OpenAI()
+
+#Response = client.responses.create(
+#    model="gpt-5",
+#    input="Write a one-sentence bedtime story about a unicorn."
+#)
+
+#print("chatgpt: " + response.output_text)
+
+##Deepseek
+#client = OpenAI(api_key=os.environ.get('sk-0b65aaa29c364d71bdb29ce86a94b1d2'), base_url="https://api.deepseek.com")
+
+#response = client.chat.completions.create(
+    #model="deepseek-chat",
+    #messages=[
+        #{"role": "system", "content": "You are a helpful assistant"},
+        #{"role": "user", "content": "Hello"},
+    #],
+    #stream=False
+#)
+
+#print(response.choices[0].message.content)
